4a0c38de7099ac4d8d908a27282fadbb0bb03987
Salva <atlas200x@gmail.com>
2025-05-22 20:29:04 -0500

    feat: search

--- btree.h
+++ btree.h
@@ -1,32 +1,130 @@
 #ifndef BTree_H
 #define BTree_H
 #include <iostream>
+#include <vector>
 #include "node.h"
 
 using namespace std;
 
-template <typename TK>
+template<typename TK>
 class BTree {
- private:
-  Node<TK>* root;
-  int M;  // grado u orden del arbol
-  int n; // total de elementos en el arbol 
+    Node<TK> *root;
+    int M; // grado u orden del arbol
+    int n; // total de elementos en el arbol
+    int total_size = 0;
 
- public:
-  BTree(int _M) : root(nullptr), M(_M) {}
 
-  bool search(TK key);//indica si se encuentra o no un elemento
-  void insert(TK key);//inserta un elemento
-  void remove(TK key);//elimina un elemento
-  int height();//altura del arbol. Considerar altura 0 para arbol vacio
-  string toString(const string& sep);  // recorrido inorder
-  vector<TK> rangeSearch(TK begin, TK end);
+    void split(Node<TK> n) {
+        auto splitted = n.split();
 
-  TK minKey();  // minimo valor de la llave en el arbol
-  TK maxKey();  // maximo valor de la llave en el arbol
-  void clear(); // eliminar todos lo elementos del arbol
-  int size(); // retorna el total de elementos insertados  
-  ~BTree();     // liberar memoria
+    }
+
+    void _inorder(Node<TK> *n, void func(Node<TK> n)) {
+        for (int i = 0; i < n->count; i++) {
+            if (!n->leaf) _inorder(n->children[i]);
+            func(n);
+        }
+        if (!n->leaf) _inorder(n->children[n->count]);
+    }
+
+public:
+    explicit BTree(int M) : root(nullptr), M(M), n(0) {
+    }
+
+    bool search(TK key) {
+        auto n = root;
+        while (n) {
+            int i = 0;
+            for (; i < n->count; ++i) {
+                auto ckey = n->keys[i];
+                if (key == ckey) {
+                    return true;
+                }
+                if (key < ckey) {
+                    break;
+                }
+            }
+
+            n = n->leaf ? nullptr : n->children[i];
+        }
+        return false;
+    }
+
+    void insert(TK key) {
+
+    }
+
+    void remove(TK key) {
+
+    }
+
+    int height() {
+        return root->height;
+    }
+
+    string toString(const string &sep) {
+        string base;
+        auto fun = [&base](Node<TK> *node) {
+            if (!base.empty()) base.push_back(*" ");
+            base.push_back(to_string(node->keys));
+        };
+        _inorder(root, fun);
+        return base;
+    }
+
+    vector<TK> rangeSearch(TK begin, TK end) {
+        return rangeSearch(root, begin, end, {});
+    }
+
+    vector<TK> rangeSearch(Node<TK> *n, TK &begin, TK &end, vector<TK> &results) {
+        int i = 0;
+        while (i < n->count) {
+            if (!n->leaf) rangeSearch(n->children[i], begin, end, results);
+
+            auto k = n->keys[i++];
+            if (k > end) {
+                break;
+            }
+
+            if (k >= begin) {
+                results.push_back(k);
+            }
+        }
+
+        if (!n->leaf) rangeSearch(n->children[i], begin, end, results);
+        return results;
+    }
+
+
+    TK minKey() {
+        return minKey(root);
+    }
+
+    TK minKey(Node<TK>* n) {
+        if (!n) throw invalid_argument("No min / empty node at minKey call");
+        return !n->leaf ? minKey(n->children[0]) : n->keys[0];
+    }
+
+    TK maxKey() {
+        return maxKey(root);
+    }
+
+    TK maxKey(Node<TK>* n) {
+        if (!n) throw invalid_argument("No max / empty node at maxKey call");
+        return !n->leaf ? maxKey(n->children[n->count]) : n->keys[n->count - 1];
+    }
+
+    void clear() {
+        root->killSelf(); // Cascade call of killSelf
+    }
+
+    int size() {
+        return total_size;
+    }
+
+    ~BTree() {
+        root->killSelf();
+    }
 };
 
 #endif
--- node.h
+++ node.h
@@ -1,30 +1,78 @@
 #ifndef NODE_H
 #define NODE_H
+#include <cmath>
 
 using namespace std;
 
-template <typename TK>
+template<typename TK>
 struct Node {
-  // array de keys
-  TK* keys;
-  // array de punteros a hijos
-  Node** children;
-  // cantidad de keys
-  int count;
-  // indicador de nodo hoja
-  bool leaf;
+    // array de keys
+    TK *keys;
+    // array de punteros a hijos
+    Node **children;
+    // cantidad de keys
+    int count, height;
+    // indicador de nodo hoja
+    bool leaf;
 
-  Node() : keys(nullptr), children(nullptr), count(0) {}
-  Node(int M) {
-    keys = new TK[M - 1];
-    children = new Node<TK>*[M];
-    count = 0;
-    leaf = true;
-  }
+    Node() : keys(nullptr), children(nullptr), count(0), height(0), leaf(false) {
+    }
 
-  void killSelf() {
-    // TODO
-  }
+    explicit Node(int M) {
+        keys = new TK[M - 1];
+        children = new Node*[M];
+        count = 0;
+        height = 0;
+        leaf = true;
+    }
+
+    template<int M>
+    explicit Node(Node* (&arr)[M]) : Node(M) {
+        for (auto &t : arr)  add_node(t);
+    }
+
+    void init(int M) {
+        keys = new TK[M - 1];
+        children = new Node*[M];
+        count = 0;
+        leaf = true;
+    }
+
+    void updateHeight(int h) {
+        height = h;
+    }
+
+    void killSelf() {
+        for (int i = 0; i < count; i++) {
+            children[i]->killSelf();
+        }
+
+        delete[] keys;
+        delete[] children;
+    }
+
+    void add_node(Node *node) {
+        if (!node) return;
+
+        children[count++] = node;
+    }
+
+    pair<Node*, Node*> split() {
+        // FIXME: implementation of Node#split
+        auto mid = static_cast<double>(count) / 2;
+        Node *left = new Node[ceil(mid)], *right = new Node[floor(mid)];
+        for (int i = 0; i < count; ++i) {
+            if (i < mid) {
+                left[i] = children[i];
+            } else {
+                right[i] ;
+            }
+
+        }
+        return {left, right};
+    }
+
+
 };
 
 #endif
